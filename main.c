a07fed0fe1b7166a64d14530fdfaea47f098379c
EduardoMatos05 <lalomatos1@gmail.com>
2024-03-17 09:53:45 -0600

    poner_piezas_con_gravedad

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #define NUM_FILAS 6
 
 void imprimirTablero(int matriz[][NUM_COLUMNAS]);
+int espacioVacioSuperior(int matriz[][NUM_COLUMNAS], int opcionColumna);
+void ponerPieza(int matriz[][NUM_COLUMNAS], int fila, int columna, int pieza);
 
 int main(int argc, char *argv[]) {
     int tablero[NUM_FILAS][NUM_COLUMNAS] = {0};
-    int juego, turno, opcion;
+    int juego, turno, opcion, filaDeFicha;
     juego = 1;
     turno = 1;
 
@@ -17,9 +19,13 @@ int main(int argc, char *argv[]) {
         if (turno % 2 == 1) {
             printf("Turno jugador 1\n");
             scanf("%i", &opcion);
+            filaDeFicha= espacioVacioSuperior(tablero, opcion);
+            ponerPieza(tablero, filaDeFicha, opcion, 1);
         } else {
             printf("Turno jugador 2\n");
             scanf("%i", &opcion);
+            filaDeFicha= espacioVacioSuperior(tablero, opcion);
+            ponerPieza(tablero, filaDeFicha, opcion, 2);
         }
 
         turno++;
@@ -30,15 +36,33 @@ int main(int argc, char *argv[]) {
 
 void imprimirTablero(int matriz[][NUM_COLUMNAS]) {
     int ii, jj;
-    for(jj=0;jj<NUM_COLUMNAS;jj++){
+    
+    for (jj = 0; jj<NUM_COLUMNAS; jj++) {
         printf(" %i", jj);
     }
     printf("\n");
-    for (ii = 0; ii < NUM_FILAS; ii++) {
+    
+    for (ii = NUM_FILAS - 1; ii >= 0; ii--) {
         printf("|");
         for (jj = 0; jj < NUM_COLUMNAS; jj++) {
             printf("%i|", matriz[ii][jj]);
         }
         printf("\n");
     }
+}
+
+
+int espacioVacioSuperior(int matriz[][NUM_COLUMNAS], int opcionColumna){
+    int ii;
+    for(ii=0; ii<NUM_FILAS; ii++){
+        if(matriz[ii][opcionColumna]==0){
+            return ii;
+        }
+    }
+    return -1;
+}
+
+void ponerPieza(int matriz[][NUM_COLUMNAS], int fila, int columna, int pieza){
+    matriz[fila][columna]= pieza;
+    return;
 }
